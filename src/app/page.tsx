8111a2edee23ffbd18fd1e2eedf44b11aba6e313
Rashedul Alam <rashedulalam112112@gmail.com>
2026-03-30 15:16:05 +0600

    feat: init shadcn installations

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-start pt-32 h-full">
+      <Button variant="default" size="lg">Hello World</Button>
+    </div>
+  );
+}
